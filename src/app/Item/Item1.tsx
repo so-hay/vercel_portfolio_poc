ee7cdb9376ab16a224012ccd6dc896ffda8e582a
so-hay <hay.soraka@gmail.com>
2025-02-17 17:03:57 +0900

    d

---
+++ src/app/Item/Item1.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Item1: React.FC = () => {
+  return(
+    <div className="text-gray-600 py-4 text-center font-mono">
+      <h3>
+        sakura
+      </h3>
+    </div>
+  )
+}
+export default Item1
